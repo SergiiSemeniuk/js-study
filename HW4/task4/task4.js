593009fe8e0cfd34212bccfa90f700c14fa31108
SergiiSemeniuk <69sem69@gmail.com>
2020-09-01 23:38:05 +0300

    4 less

---
+++ HW4/task4/task4.js
@@ -0,0 +1,10 @@
+window.addEventListener('click', handler);
+
+function handler(event) {
+	const element = event.toElement;
+    if(element.tagName !== 'TD') {
+        return;
+    }
+    const color = element.style.background;
+    document.body.style.backgroundColor = color;
+}
